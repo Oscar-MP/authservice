d6719364eb8bbdbb08bc21adaf7367ab7505cf13
MrSleider <thegamingesp56@gmail.com>
2021-02-21 18:46:25 +0100

    Validadores

---
+++ src/common/validators/user.validator.js
@@ -0,0 +1,25 @@
+'use strict'
+
+var utils = require('../utils.js');
+
+module.exports = {
+  validate_http_user_data: (req, res, next) => {
+      // Checks if the provided data is valid for a user. Does not perform any search operation nor
+      // validate the user. Only checks if there is enough data to move to the next layer
+      var params = req.body;
+console.log(params)
+      if (
+        utils.isEmpty(params.username)                                          ||
+        (utils.isEmpty(params.email) || !utils.validator.isEmail(params.email)) ||
+        utils.isEmpty(params.password)
+      ) {
+        res.status(400).send({
+          message: 'Could not validate the provided user data'
+        });
+      }
+
+      next();
+
+  },
+
+}
