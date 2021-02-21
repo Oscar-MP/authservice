a30198b95b9886cc35424701e568c721e35756d8
MrSleider <thegamingesp56@gmail.com>
2021-02-21 18:53:04 +0100

    Añadido sericio para autenticar

---
+++ src/services/auth.service.js
@@ -0,0 +1,19 @@
+'use strict'
+var User = require('../models/user.model.js');
+
+class AuthService {
+
+  constructor () {}
+
+  async SignUp ( data ) {
+
+  }
+
+  async SignIn () {
+
+  }
+
+  async SignOut () {
+
+  }
+}
