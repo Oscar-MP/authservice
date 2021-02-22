e887dc30e981ac565dea732796fda12a671d224f
MrSleider <thegamingesp56@gmail.com>
2021-02-22 20:30:19 +0100

    Mejorados los mensajes de arranque

--- index.js
+++ index.js
@@ -6,22 +6,25 @@
  *  Desription:
  */
 
-var mongo       = require('./config/mongodb.config.js');
 const env       = 'dev'; // This will be implemented somewhere else
 const messages  = require('./src/common/messages.js');
 const chalk     = require('chalk');
 
-const config = require('./config/environment.js').getConfig(env); //dev
-
 messages.info('Iniciando aplicación...');
 
-const { server } = require('./src/server.js');
+var mongo       = require('./config/mongodb.config.js');
+const config = require('./config/environment.js').getConfig(env); //dev
 
+
+const { server } = require('./src/server.js');
 
 server.listen( config.PORT )
   .on('listening', () => {
     // Everything went well, the server is up and running
-    console.log(chalk.bgBlue.green(`[*] Aplicación funcionando sin problemas:\n- PUERTO: ${config.PORT}\n- MODO: ${env}`))
+    console.log(chalk.bgBlue.green(chalk.bold(`\n[*] Servidor HTTP funcionando sin problemas:\n`) +
+                                    `- PUERTO: ${config.PORT}\n` +
+                                    `- MODO: ${env}\n` +
+                                    `- DB: ${mongo.isConnected()}\n`))
   })
   .on('error', (err) => {
     console.log('[!] Error al iniciar la aplicación');
