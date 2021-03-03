11989bed814207ae679c26fdcab6a680f44dab58
MrSleider <thegamingesp56@gmail.com>
2021-03-03 19:14:17 +0100

    Añadida gestion para centralizar errores y usar una clase para las respuestas

--- src/common/helpers/error.js
+++ src/common/helpers/error.js
@@ -3,9 +3,37 @@
 class ErrorHandler extends Error {
   constructor (status, message) {
     super();
-    this.status   = status;
-    this.message  = message;
+    this.status     = status      || 500;
+    this.message    = message     || 'Unexpected error';
+
   }
+
+  send ( res ) {
+    // This method will terminate the HTTP connection by responding with the error
+    res.status(this.status).json({
+      message: this.status !== 500 ? this.message : 'Internal Server Error!',
+      code: this.status
+    });
+  }
+
+  static get_status_code_message ( code ) {
+
+  }
+}
+
+class HTTP_Error extends ErrorHandler {
+  constructor(status, message) {
+    super(status, message);
+
+  }
+}
+
+const HTTP_STATUS_CODES = {
+  OK: 200,
+  BAD_REQUEST: 400,
+  UNAUTHORIZED: 401,
+  NOT_FOUND: 404,
+  INTERNAL_SERVER_ERROR: 500
 }
 
 module.exports =  { ErrorHandler };
--- src/common/helpers/response.js
+++ src/common/helpers/response.js
@@ -1,6 +1,28 @@
 'use strict'
 
-module.exports = {
-  error: () => {},
-  res: () => {}
+const { ErrorHandler } = require('./error.js');
+
+class Response {
+  // A response class for success requests
+  constructor() {
+      this.status   = 200;
+      this.message  = 'The request done successfully';
+  }
+
+  send (res, message, data) {
+    message = message || this.message;
+
+    try {
+      return res.status(200).json({
+        message: message,
+        data: data
+      });
+    } catch (e) {
+      throw new ErrorHandler(500, 'Error trying to send the response', e);
+    }
+  }
+
+
 }
+
+module.exports = { Response: new Response() };
