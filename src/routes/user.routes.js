236a7ef6f0f9f5a8667a1e1350088c3bd71cf11e
MrSleider <thegamingesp56@gmail.com>
2021-03-07 18:41:50 +0100

    Añadido el nuevo validador  para validar los IDs

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,13 +1,26 @@
-var express       = require('express');
-const controller  = require('../controllers/user.controller.js');
+var express           = require('express');
+const controller      = require('../controllers/user.controller.js');
+const paramValidator  = require('../common/validators/param.validator.js');
 
 var router = express.Router();
 
 // We should validate first that the user has enough permissions to list users
 router.get('/users', controller.get_users);
-router.get('/user/:id', controller.get_user);
-router.get('/user/:id/history', controller.get_user_history);
-router.post('/user/:id/update', controller.update_user);
-router.delete('/user/:id', controller.remove_user);
+router.get('/user/:id',
+            paramValidator.is_a_valid_id,
+            controller.get_user
+          );
+router.get('/user/:id/history',
+            paramValidator.is_a_valid_id,
+            controller.get_user_history
+          );
+router.post('/user/:id/update',
+            paramValidator.is_a_valid_id,
+            controller.update_user
+          );
+router.delete('/user/:id',
+              paramValidator.is_a_valid_id,
+              controller.remove_user
+            );
 
 module.exports = router;
