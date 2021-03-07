be8e5834c88bf85cbb25b568e6d32bd8efbbc0c4
MrSleider <thegamingesp56@gmail.com>
2021-03-07 18:40:48 +0100

    Añadida estructura básica del crud de roles

---
+++ src/controllers/role.controller.js
@@ -0,0 +1,33 @@
+'use strict'
+
+const service           = require('../services/role.service.js');
+const { ErrorHandler }  = require('../common/helpers/error.js');
+const { Logger }        = require('../common/helpers/logger.js');
+
+async list_roles () {
+  // Lists all the roles
+}
+
+async get_role() {
+  // Get a specific role
+}
+
+async create_role() {
+
+}
+
+async update_role() {
+
+}
+
+async remove_role() {
+
+}
+
+module.exports = {
+  list_roles,
+  get_role,
+  create_role,
+  update_role,
+  remove_role
+}
