1dffa0ce55b9f475b3307eff930e7a39d5d0e03e
MrSleider <thegamingesp56@gmail.com>
2021-03-09 20:10:30 +0100

    Añadido indice para importar todos los validadores

---
+++ src/common/validators/index.js
@@ -0,0 +1,5 @@
+module.exports = {
+  ParamValidator: require('./param.validator.js'),
+  PermissionValidator: require('./permissions.validator.js'),
+  UserValidator: require('./user.validator.js')
+};
