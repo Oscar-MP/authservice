85f71996ecadd2871fac045f292313a132f3d246
MrSleider <thegamingesp56@gmail.com>
2021-03-09 20:09:59 +0100

    Modificados el import y el nombre de un validador en las rutas de usuarios

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,25 +1,26 @@
 var express           = require('express');
 const controller      = require('../controllers/user.controller.js');
-const paramValidator  = require('../common/validators/param.validator.js');
+const { PermissionValidator, ParamValidator } = require('../common/validators/index.js');
+
 
 var router = express.Router();
 
 // We should validate first that the user has enough permissions to list users
 router.get('/users', controller.get_users);
 router.get('/user/:id',
-            paramValidator.is_a_valid_id,
+            ParamValidator.is_a_valid_id,
             controller.get_user
           );
 router.get('/user/:id/history',
-            paramValidator.is_a_valid_id,
+            ParamValidator.is_a_valid_id,
             controller.get_user_history
           );
 router.post('/user/:id/update',
-            paramValidator.is_a_valid_id,
+            ParamValidator.is_a_valid_id,
             controller.update_user
           );
 router.delete('/user/:id',
-              paramValidator.is_a_valid_id,
+              ParamValidator.is_a_valid_id,
               controller.remove_user
             );
 
