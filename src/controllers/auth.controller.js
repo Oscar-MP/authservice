08133e660aa7059e161984f1f8b4a6e646e025d8
MrSleider <thegamingesp56@gmail.com>
2021-03-14 21:46:23 +0100

    Añadida gestión para activar las cuentas de usuario después del signup

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,18 +1,17 @@
 const service = require('../services/auth.service.js');
-const { ErrorHandler }  = require('../common/helpers/error.js');
-const { Logger }        = require('../common/helpers/logger.js');
-
+const { ErrorHandler, Logger, Response } = require('../common/helpers/index.js');
 
 module.exports = {
   signup: async (req, res, next) => {
     const params = req.body;
 
     try {
-      const registrationData = await service.SignUp(params);
+      const { registrationData, activation_link } = await service.SignUp(params);
 
       return res.status(200).send({
         message: 'The user has been created!',
-        data: registrationData
+        data: registrationData,
+        activation_link: activation_link
       });
     } catch ( err ) {
       next(ErrorHandler.stack(err, 'Could not sign up'));
@@ -40,6 +39,26 @@ module.exports = {
     }
   },
   logout: async (req, res, next) => {
+
+  },
+  activate_account: async (req, res, next) => {
+    // Once an email has been sent after the signup, the user must follow a link which will proceed with the
+    // final step of the account verification. We will take the userid and the activation id and if everything matches we will activate
+    // the account.
+    // * An acctivation ID must be created during the signup
+    const { randomSeed, userid, activation_id } = req.params;
+
+    try {
+      if ( await service.activateAccount(activation_id, userid) !== true ) {
+        throw new ErrorHandler(500, 'Could not proceed with the user activation', { print: false });
+      }
+
+      return Response.send(res, 'Your user has been successfully activated! Now you can login.');
+
+    } catch ( e ) {
+      next(e);
+    }
+
 
   }
 };
