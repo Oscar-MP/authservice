cd60a25c51c8d95d47c46eb7f6cf27d90cffe305
MrSleider <thegamingesp56@gmail.com>
2021-03-18 21:19:43 +0100

    Añadido un comentario

--- src/common/middlewares/error-handler.middleware.js
+++ src/common/middlewares/error-handler.middleware.js
@@ -5,6 +5,7 @@ var handleError = (err, req, res, next) => {
   // This is the centralized error handler for the API
 
   if (res.headersSent) {
+
     return next(err)
   }
 
@@ -12,15 +13,17 @@ var handleError = (err, req, res, next) => {
     return err.send(res);
   } else {
 
-    let message = err.status >= 500 || !err.status ? 'Internal Server Error!' : (err.message || err._message) || 'Unexpected error happened!';
-    return res.status( err.status ? err.status : 500).send({
-      message: message
-    });
+  // Esto se debe modificar!!!
+  let message = err.status >= 500 || !err.status ? 'Internal Server Error!' : (err.message || err._message) || 'Unexpected error happened!';
+  return res.status( err.status ? err.status : 500).send({
+    message: message
+  });
   }
 }
 
 var logError = (err, req, res, next) => {
   // Logs every error
+
   if (err.print)
     Logger.error('An error has been intercepted by the controller', err);
   return next(err);
