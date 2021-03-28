ba1b86de583c419975bcc566540ada7e4c924ed0
MrSleider <thegamingesp56@gmail.com>
2021-03-28 17:48:55 +0200

    Modificados los datos del modelo session

--- src/models/session.model.js
+++ src/models/session.model.js
@@ -2,6 +2,7 @@
 
 var mongoose  = require('mongoose');
 var Schema    = mongoose.Schema;
+const { ErrorHandler } = require('../common/helpers');
 
 const schema = new Schema({
 
@@ -22,20 +23,42 @@ const schema = new Schema({
     type: String,
     required: [ true, 'A token must be provided']
   },
-  client_info: {
-    ip: String,
-    resolution: String,
-    browser: [String],
+  token_secret: {
+    type: String
   },
   lang: {
     type: String,
     default: 'ES'
   },
-  history: [String],
-  expiration_date: Date // Tiempo de vida de la sesion
+  history: [ String ],
+  expiration_date: {
+    type: Date,
+    default: function () {
+      let addition_time = 1000 /* miliseconds */
+                          * 60 /* seconds */
+                          * 60 /* minutes */
+                          * 24 /* hours */
+                          * 7  /* days */;
+      var now = new Date().getTime();
+
+      return new Date(now + addition_time);
+    }
+  },
+  last_join: {
+    type: Date,
+    default: Date.now
+  }
 });
 
 schema.static.get_active_session = ( userid ) => {
   // Obtains the active session of a user
 }
+
+
+schema.pre('save', async function (next) {
+    // Let's check if the user does exist
+
+});
+
+
 module.exports = mongoose.model('Session', schema);
