7b773ceb5c3839a2add09e8d5900a7679d79749b
MrSleider <thegamingesp56@gmail.com>
2021-04-07 19:42:52 +0200

    Modificada la información del token (untested) este modelo será cambiado por el JWT

--- src/models/token.model.js
+++ src/models/token.model.js
@@ -1,15 +1,20 @@
 'use strict';
+/**
+  * This file will be changed with the JWT implementation
+  */
+
 const { Utils } = require('../common/lib');
 
 class Token {
-  constructor(userid, secret) {
-    this.secret = secret;
-    this.userid = userid;
+  constructor(secret, payload) {
+    this.secret     = secret;
+    this.userid     = payload.userid;
+    this.sessionid  = payload.sessionId;
 
   }
 
   craft() {
-    return `${this.secret}.${Utils.getRandomStr(8)}.${this.userid}`;
+    return `${this.sessionid}.${Utils.getRandomStr(8)}.${this.userid}`;
   }
 }
 
