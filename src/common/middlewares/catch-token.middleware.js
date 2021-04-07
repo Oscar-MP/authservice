73411ac9047ae788dbd6bd680866c159537fee28
MrSleider <thegamingesp56@gmail.com>
2021-04-07 21:28:09 +0200

    Añadida funcionalidad para capturar el token de los headers o del body y añadirlo al objeto request

--- src/common/middlewares/catch-token.middleware.js
+++ src/common/middlewares/catch-token.middleware.js
@@ -6,15 +6,23 @@
  *              request header, verify that is a well-crafted token and append it into the request object
  */
 
-const { ErrorHandler } = require('../helpers')
+const { ErrorHandler } = require('../helpers');
+const { Utils } = require('../lib');
 
-module.exports.catchToken = (req, res, next) => {
+module.exports.catch_token = async (req, res, next) => {
 
-  const authheaders = req.headers;
+  const authHeader = req.headers['authorization'];
 
-  if (headers.startsWith("Bearer ")) {
-    let token = headers.substring
+  if ( !Utils.isEmpty(authHeader) && authHeader.split(' ')[0] === 'Bearer' ) {
+    req.token = authHeader.split(' ')[1];
+  } else if ( req.body.token ) {
+    req.token = req.body.token;
   } else {
-    return next(new ErrorHandler(401, `There is no authorization header set`));
+    return res.status(403).send({
+      message: `You don't have permissions to access this route`
+    });
   }
+
+  // Once we have the token in the request we will check that is well crafted
+  next();
 }
