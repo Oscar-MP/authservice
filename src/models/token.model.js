c6bdd3ac51ed3543b3a8d2de18f231b3858a5ecc
MrSleider <thegamingesp56@gmail.com>
2021-04-08 23:18:38 +0200

    Refactorizado el token para añadir tokens jwt

--- src/models/token.model.js
+++ src/models/token.model.js
@@ -4,17 +4,66 @@
   */
 
 const { Utils } = require('../common/lib');
+const { ErrorHandler } = require('../common/helpers');
+
+const jwt    = require('jsonwebtoken');
+const Config = require('../../config/config.js');
+var ObjectId = require('mongoose').Types.ObjectId;
 
 class Token {
-  constructor(secret, payload) {
-    this.secret     = secret;
-    this.userid     = payload.userid;
-    this.sessionid  = payload.sessionId;
+  constructor( payload, secret, token_secret ) {
+
+    if (!payload.userId || !payload.username ) {
+      throw new ErrorHandler(500, `Missing token information`);
+      return;
+    }
+    if ( secret === true ) {
+      this.secret   = token_secret || Utils.getRandomStr(12);
+    }
+
+    this.userId     = payload.userId;
+    this.sessionId  = payload.sessionId || ObjectId();
+    this.seed       = Utils.getRandomStr(8);
+    this.username   = payload.username;
+  }
+
+  encode() {
+    // Encodes the token into a jwt signed token.
+
+    let payload = {
+      userId: this.userId,
+      sessionId: this.sessionId,
+      username: this.username,
+      seed: this.seed
+    };
+
+    try {
+      if (!this.secret) throw new ErrorHandler(500, `No token secret specified`);
+
+      return jwt.sign( payload, this.secret, {
+        expiresIn: Config.getConfig().token_ttl
+      });
+    } catch ( error ) {
+      throw ErrorHandler.stack(error, 'Could not encode the token')
+    }
+  }
+
+  static decode( token_string ) {
+    // Decodes the token payload and returns a token object with it
+  }
+
+  static verify ( token_string, secret ) {
+    // Verifies the token signature and if the token is alive. Returns a bool?
 
   }
 
-  craft() {
-    return `${this.sessionid}.${Utils.getRandomStr(8)}.${this.userid}`;
+  static isValid ( token ) {
+    // Checks if the token is well crafted. This method does not verify the token signature.
+    // Return a bool
+  }
+
+  static isAlive( token ) {
+
   }
 }
 
