1c2e1cefcfa338c75eeeb21e83d40ff07c9a330e
MrSleider <thegamingesp56@gmail.com>
2021-04-08 23:17:33 +0200

    Añadidas configuraciones para el token y las sesiones

--- config/environments/dev.js
+++ config/environments/dev.js
@@ -6,5 +6,7 @@ module.exports = {
     database_name: 'auth'
   },
   port: 7850,
-  log_errors: true
+  log_errors: true,
+  token_ttl: 60 * 60 * 6, // 6 Hours in seconds
+  session_ttl: 1000 * 60 * 60 * 24 * 7 // 7 Days in miliseconds
 }
