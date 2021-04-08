b2af8e0a36acece020bdce744a333acec88b8d10
MrSleider <thegamingesp56@gmail.com>
2021-04-08 23:17:58 +0200

    Modificado ( unfinished, untested )

--- src/common/middlewares/catch-session.middleware.js
+++ src/common/middlewares/catch-session.middleware.js
@@ -1,12 +1,20 @@
 'use strict'
 
 const { ErrorHandler } = require('../helpers');
+const { Token } = require('../../models/token.model');
 
 module.exports.catch_session = async (req, res, next) => {
   // Retrieves the token from the headers, then obtains the session related with that token
   // Verifies the token sign HMAC and once everything is checked appends the session information
   // to the request object
-  console.log('Token: ', req.token);
+  if (!req.token) {
+    // Something failed in the previous error propagation
+    next(new ErrorHandler(400, `You don't have access to this API. Invalid token or not provided.`));
+  }
+
+  // Decode the token and verify
+
+
   next();
 
 
