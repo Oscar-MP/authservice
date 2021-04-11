b52235574d83b629273865ef0fc3b416c3e805bf
MrSleider <thegamingesp56@gmail.com>
2021-04-11 02:23:23 +0200

    Añadida la autorización de rutas

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,25 +1,29 @@
 var express           = require('express');
 const controller      = require('../controllers/user.controller.js');
 const { PermissionValidator, ParamValidator } = require('../common/validators');
-
+const { permissions } = require('../common/middlewares');
 
 var router = express.Router();
 
 // We should validate first that the user has enough permissions to list users
 router.get('/users', controller.get_users);
 router.get('/user/:id',
+            permissions.required_permissions(45),
             ParamValidator.is_a_valid_id,
             controller.get_user
           );
 router.get('/user/:id/history',
+            permissions.required_permissions(85),
             ParamValidator.is_a_valid_id,
             controller.get_user_history
           );
 router.post('/user/:id/update',
+            permissions.required_permissions(75),
             ParamValidator.is_a_valid_id,
             controller.update_user
           );
 router.delete('/user/:id',
+              permissions.required_permissions(75),
               ParamValidator.is_a_valid_id,
               controller.remove_user
             );
